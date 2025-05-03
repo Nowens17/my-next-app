0d5cb801926108abf413f79e51980e93cac6f15e
nick-bgl <nick@bitgraphlabs.co>
2025-05-03 07:54:25 -0500

    committing initial updates

---
+++ app/ui/fonts.ts
@@ -0,0 +1,19 @@
+import { Inter, Poppins, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter(
+    { 
+        subsets: ['latin'],
+        weight: ['400', '500', '600', '700'] }
+);
+export const poppins = Poppins(
+    { 
+        subsets: ['latin'],
+        weight: ['400', '500', '600', '700'] 
+    }
+);
+export const lusitana = Lusitana(
+    { 
+        subsets: ['latin'],
+        weight: ['400', '700'] 
+    }
+);
